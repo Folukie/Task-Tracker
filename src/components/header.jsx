d81cf1a57f54a80208667c406d93e5147fce1f6a
Foluke Odusanya <odusanya.folukemi@gmail.com>
2021-09-11 00:55:55 +0100

    add footer section

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import Button from "./button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        onClick={onAdd}
+        text={showAdd ? "Close" : "Add"}
+        color={showAdd ? "red" : "green"}
+      />
+    </header>
+  );
+};
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
